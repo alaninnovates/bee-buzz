7a614fcdd22213c574e53700a3ebe63391513e48
alaninnovates <alanlichen1@gmail.com>
2025-07-17 09:56:03 -0400

    Implement leaderboard

---
+++ src/commands/leaderboard.ts
@@ -0,0 +1,70 @@
+import { Command } from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
+import { EmbedBuilder } from 'discord.js';
+
+const topEmojis = ['🥇', '🥈', '🥉'];
+
+@ApplyOptions<Command.Options>({
+    name: 'leaderboard',
+    description: 'View top players',
+})
+export class LeaderboardCommand extends Command {
+    public override registerApplicationCommands(registry: Command.Registry) {
+        registry.registerChatInputCommand(
+            (builder) =>
+                builder.setName(this.name).setDescription(this.description),
+            {
+                idHints: ['1395401934503940098'],
+            },
+        );
+    }
+
+    public override async chatInputRun(
+        interaction: Command.ChatInputCommandInteraction,
+    ) {
+        const hives = await this.container.database
+            .collection('hives')
+            .find({})
+            .sort({ honey: -1 })
+            .limit(10)
+            .toArray();
+
+        if (hives.length === 0) {
+            await interaction.reply({
+                content: 'No players found in the leaderboard.',
+                ephemeral: true,
+            });
+            return;
+        }
+
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle('🏆 Top Players - Honey')
+                    .setColor('#FFD700')
+                    .setDescription(
+                        (
+                            await Promise.all(
+                                hives
+                                    .slice(0, 10)
+                                    .map(
+                                        async (hive, index) =>
+                                            `${
+                                                index < 3
+                                                    ? topEmojis[index]
+                                                    : `${index + 1}. `
+                                            } ${
+                                                (
+                                                    await this.container.client.users.fetch(
+                                                        hives[0].userId,
+                                                    )
+                                                ).displayName
+                                            } - 🍯 ${hive.honey} jars`,
+                                    ),
+                            )
+                        ).join('\n'),
+                    ),
+            ],
+        });
+    }
+}
